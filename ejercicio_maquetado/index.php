4bd903f9cc05d312ecb041b908f386a906f85a12
rodrigo-frenk <rodrigo.frenk@gmail.com>
2017-01-14 15:16:15 -0600

    correccion ejemplo bootstrap

--- ejercicio_maquetado/index.php
+++ ejercicio_maquetado/index.php
@@ -7,6 +7,8 @@ include_once 'componentes/ejercicio1/01-posicionamiento.php';
 
 include_once 'componentes/ejercicio1/02-absoluto.php';
 
+include_once 'componentes/ejercicio1/03-reticula-bootstrap.php';
+
 
 include_once 'footer.php';
 
